8a315dcd240ee486a420e044cae089e46910d08b
Suxrob Sagdullayev <suxrobsagdullayev5@gmail.com>
2025-12-11 19:11:47 +0500

    If, else.

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+// If, else. Shartlar
+
+/* if - agar  */
+/* elseif - yoki  */
+/* else - aks-holda */
+
+$x = 10;
+$y = 20;
+if ($x == $y){
+    echo "x teng y";
+} 
+elseif ($x > $y){
+    echo "x katta y";
+} 
+else {
+    echo "x kichik y";
+}
+
+
+?>
